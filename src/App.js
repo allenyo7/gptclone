0b7bcfa124916de605a6acd10c3d687824b917d8
Allen Paty <allenpatypersonal@gmail.com>
2024-07-30 15:05:52 -0600

    I added all of the css and app.js divs to make this version render. it is the version that only has all the layout set up, but nothing works yet.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./normal.css"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          <span>+</span>
+          New Chat
+        </div>
+      </aside>
+
+      <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="chat-message-center">
+                  <div className="Avatar-me">
+                    Me
+                  </div>
+                  <div className="message">
+                    Hello World
+                  </div>
+            </div>
+          </div>
+
+          <div className="chat-message chatgpt">
+            <div className="chat-message-center">
+                  <div className="Avatar-ai">
+                    AI
+                  </div>
+                  <div className="message">
+                    Hello, I am C3PO human cyborg relations, how can I help you?
+                  </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="chat-input-holder">
+          <textarea
+            rows="1"
+            className="chat-input-textarea"
+            placeholder="Type your message to Blallen here"
+          ></textarea>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
